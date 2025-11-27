58382b5a6150b935c03aa276ac64efb5bf637674
ibktommy <atomquartz@gmail.com>
2025-11-27 03:32:53 +0100

    begin ai chatbot project

---
+++ ai_chatbot/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ChatBotStart from "./Components/ChatBotStart/ChatBotStart";
+
+const App = () => {
+  return (
+    <div className="container">
+      <ChatBotStart />
+    </div>
+  );
+};
+
+export default App;
---
+++ ai_chatbot/src/Components/ChatBotStart/ChatBotStart.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ChatBotStart from '../ChatBotStart/ChatBotStart.css'
+
+const ChatBotStart = () => {
+  return (
+    <div className="start_page">
+      <button className="start_page_btn">Click to start Chat with AI</button>
+    </div>
+  );
+};
+
+export default ChatBotStart;
